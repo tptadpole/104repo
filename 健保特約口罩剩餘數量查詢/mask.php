8b02627c98849f14d147d72e9a76a7f542b02ac1
tptadpole <79893601+tptadpole@users.noreply.github.com>
2021-03-11 13:20:30 +0800

    Add files via upload

---
+++ 健保特約口罩剩餘數量查詢/mask.php
@@ -0,0 +1,58 @@
+<?php
+
+function cmp($a,$b)
+{
+
+    if ( mb_strlen($a[2], "UTF-8") != strlen($a[2]) )
+        return -1 ;
+
+    if ( (int)$a[2] == (int)$b[2] ) return 0 ;
+    return( (int)$a[2] > (int)$b[2] ) ? -1 : 1 ;
+}
+function run( $input ) {
+
+    require_once('vendor/autoload.php') ;
+    $climate = new League\CLImate\CLImate;
+
+    $csvData = file_get_contents('https://data.nhi.gov.tw/Datasets/Download.ashx?rid=A21030000I-D50001-001&l=https://data.nhi.gov.tw/resource/mask/maskdata.csv');
+    $lines = explode(PHP_EOL, $csvData) ;
+    $array = array() ;
+
+    $user_input = $input;
+
+    for( $i = 0 ; $i < count($lines)-1; $i++ ) {
+        $array[$i] = str_getcsv($lines[$i]);
+
+        if ( $i == 0 || ( mb_strpos( $array[$i][2], $user_input,0,"utf-8" ) !== false ) ){
+            // 去除不需要的資訊
+            array_splice($array[$i],5,2); 
+            array_splice($array[$i],3,1);
+            array_splice($array[$i],0,1);
+        } 
+        else {
+            array_splice($array,-1,1) ;
+        } 
+
+
+    }
+    
+    usort($array,"cmp");
+    if ( $array != [] ) {       
+        $climate->table($array);
+    }
+    else{
+        echo"找不到結果\n";
+    }
+    
+}
+
+while ( true ) {
+
+    $input = readline('請輸入要尋找口罩的地區:');
+    run($input) ;
+
+    $input = readline("\n離開請輸入exit\n");
+    if ( strcmp($input,'exit') === 0 ){
+        break;
+    }
+}
